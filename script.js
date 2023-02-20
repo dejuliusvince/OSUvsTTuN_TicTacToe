7205df922e1502391cc82c5896f6cb46176ff9e3
vincedejulius <dejuliusvince@gmail.com>
2023-02-20 11:00:04 -0500

    function to begin game with osu turn, set initial state of baord. begin arrays of win states on board

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 
 const OSU_CLASS = 'osu'
 const MICH_CLASS = 'mich'
-const cellElements = document.querySelectorAll('[data-cell')
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
 let michTurn
 
 //click event will only fire once for each cell during the 
 //game, preventing multiple clicks on the same cell
-cellElements.forEach(cell => {
-  cell.addEventListener('click', handleClick, { once: true})
-})
+
+startGame()
+
+function startGame() {
+  michTurn = false
+  cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true})
+  })
+  setBoardHoverClass()
+}
 
 function handleClick(e){
   const cell = e.target
@@ -17,8 +31,26 @@ function handleClick(e){
   // Check For Win
   // Check For Draw
   // Switch Turns
+  swapTurns()
+  setBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass)
 }
+
+function swapTurns() {
+  michTurn = !michTurn
+}
+
+function setBoardHoverClass() {
+  
+board.classList.remove(OSU_CLASS)
+  board.classList.remove(MICH_CLASS)
+  if (michTurn) {
+    board.classList.add(MICH_CLASS)
+  }
+  else {
+    board.classList.add(OSU_CLASS)
+  }
+};
